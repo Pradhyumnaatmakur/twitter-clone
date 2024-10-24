9a9a6c3e350f2226af891d9d7217f13e30017b16
pradhyumna <pradhyumnaatmakur1@gmail.com>
2024-10-24 09:04:23 +0530

    backend auth

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -5,7 +5,7 @@ import {
   logout,
   getMe,
 } from "../controllers/auth.controller.js";
-import protectRoute from "../middleware/protectRoute.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
